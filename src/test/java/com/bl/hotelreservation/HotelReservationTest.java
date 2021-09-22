cc2e9e79e8c1b6a1c0bd0bc11a23fd0d8ee75364
jumdemonali <jumdemonali@gmail.com>
2021-09-22 18:35:02 +0530

    Modified: Ability to add hotel reservation system with name and rates

--- src/test/java/com/bl/hotelreservation/HotelReservationTest.java
+++ src/test/java/com/bl/hotelreservation/HotelReservationTest.java
@@ -1,12 +1,21 @@
 package com.bl.hotelreservation;
 
-import org.junit.Test;
-import static org.junit.Assert.assertTrue;
+import org.junit.Test;;
+import org.junit.Assert;
+
 public class HotelReservationTest {
     @Test
-    public void WhenConditionMeetShouldAnswerWithTrue()
-    {
-        assertTrue( true );
+    public void givenHotelDetails_WhenValuesEnteredCorrect_ShoulReturnTrue() {
+        HotelReservationSystem hotelReservationService = new HotelReservationSystem();
+        boolean isValidHotel1 = hotelReservationService.addHotel("Lakewood", 3, 110, 90);
+        Assert.assertEquals(true, isValidHotel1);
+    }
+
+    @Test
+    public void GivenHotelDetails_WhenValuesEnteredIncorrect_ShoulReturnFalse() {
+        HotelReservationSystem hotelReservationService = new HotelReservationSystem();
+        boolean isValidHotel1 = hotelReservationService.addHotel(null, 3, 110, 90);
+        Assert.assertEquals(false, isValidHotel1);
     }
 
 }
