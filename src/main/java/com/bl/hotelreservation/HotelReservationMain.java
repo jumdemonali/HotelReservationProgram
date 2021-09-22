63bde730a76fde1bbf47cab93d06077a8e23f68b
jumdemonali <jumdemonali@gmail.com>
2021-09-22 08:14:02 +0530

    Modified: Refactor Class name

--- src/main/java/com/bl/hotelreservation/HotelReservationMain.java
+++ src/main/java/com/bl/hotelreservation/HotelReservationMain.java
@@ -1,6 +1,6 @@
 package com.bl.hotelreservation;
 
-public class hotelreservationmain {
+public class HotelReservationMain {
     public static void main(String[] args) {
         System.out.println("Welcome to hotel reservation project!!");
     }
