e19bbddff08c455646d8be932c7667d17cb014f3
jumdemonali <jumdemonali@gmail.com>
2021-09-22 08:10:36 +0530

    Added: Welcome message

---
+++ src/main/java/com/bl/hotelreservation/hotelreservationmain.java
@@ -0,0 +1,7 @@
+package com.bl.hotelreservation;
+
+public class hotelreservationmain {
+    public static void main(String[] args) {
+        System.out.println("Welcome to hotel reservation project!!");
+    }
+}
