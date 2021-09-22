9a2330b19fbe3661c2ff8fd18a184f1643af5519
jumdemonali <jumdemonali@gmail.com>
2021-09-22 19:18:12 +0530

    Added: Ability to find cheapest hotel for given date range

--- src/main/java/com/bl/hotelreservation/HotelReservationSystem.java
+++ src/main/java/com/bl/hotelreservation/HotelReservationSystem.java
@@ -1,8 +1,16 @@
 package com.bl.hotelreservation;
 import java.util.ArrayList;
+import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.Comparator;
+import java.util.Optional;
+
 public class HotelReservationSystem {
     ArrayList<Hotel> hotelList = new ArrayList<Hotel>();
     Hotel hotel;
+    Scanner sc=new Scanner(System.in);
     public boolean addHotel(String hotelName, int rating,double weekdayRegularCustomerCost,double weekendRateRegular) {
        hotel=new Hotel();
         hotel.setHotelName(hotelName);
@@ -12,6 +20,13 @@ public class HotelReservationSystem {
         hotelList.add(hotel);
         return true;
     }
+    public Hotel getCheapestHotel(LocalDate startDate, LocalDate endDate) {
+
+        long numberOfDays = ChronoUnit.DAYS.between(startDate, endDate);
+        Optional<Hotel> sortedHotelList = hotelList.stream().min(Comparator.comparing(Hotel::getWeekdayRegularCustomerCost));
+        return sortedHotelList.get();
+    }
+
 }
 
 
--- src/test/java/com/bl/hotelreservation/HotelReservationTest.java
+++ src/test/java/com/bl/hotelreservation/HotelReservationTest.java
@@ -2,9 +2,13 @@ package com.bl.hotelreservation;
 
 import org.junit.Test;;
 import org.junit.Assert;
+
+import java.time.LocalDate;
+import java.time.Month;
+
 public class HotelReservationTest {
     @Test
-    public void givenHotelDetails_WhenValuesEnteredCorrect_ShoulReturnTrue() {
+    public void GivenHotelDetails_WhenValuesEnteredCorrect_ShoulReturnTrue() {
         HotelReservationSystem hotelReservationService = new HotelReservationSystem();
         boolean isValidHotel1 = hotelReservationService.addHotel("Lakewood", 3, 110, 90);
         Assert.assertEquals(true,isValidHotel1);
@@ -15,5 +19,16 @@ public class HotelReservationTest {
         boolean isValidHotel1 = hotelReservationService.addHotel(null,3,110,90);
         Assert.assertEquals(false,isValidHotel1);
     }
+    @Test
+    public void GivenHotelDetails_shouldReturnCheapestHotel(){
 
+        HotelReservationSystem hotelReservation = new HotelReservationSystem();
+        hotelReservation.addHotel("Bridgewood", 4, 160,110);
+        hotelReservation.addHotel("Ridgewood", 5, 220,40);
+        LocalDate startDate = LocalDate.of(2021, Month.SEPTEMBER, 10);
+        LocalDate endDate = LocalDate.of(2021, Month.SEPTEMBER, 11);
+        Hotel hotel = hotelReservation.getCheapestHotel(startDate, endDate);
+        Assert.assertEquals("Bridgewood", hotel.getHotelName());
+
+    }
 }
